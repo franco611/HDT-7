8c8aa2b7b1cfeb6592ca31cae9a038c53e350036
franco611 <62269078+franco611@users.noreply.github.com>
2021-04-14 16:26:18 -0600

    Modificacion del Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,55 @@
-public class Main {
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.BufferedWriter;
+
+class Main {
+    public static void main(String[]args){
+
+
+
+        int res = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Bienvenido lector de cartas ");
+        System.out.println("Seleccione la implementacion que desea utizar para el stack: ");
+        System.out.println("[ 1 ] HashMap.");
+        System.out.println("[ 2 ] TreeMap.");
+        System.out.println("[ 3 ] LinkedHashMap.");
+
+        while(true){
+            try{
+                res = s.nextInt();
+
+                if(res>=1 && res<=3){
+                    break;
+                }else{
+                    System.out.println("Ingrese valores entre 1 y 3 !");
+                }
+            }catch(Exception e){
+                System.out.println("Ingrese valores numericos ! ! !");
+            }
+        }
+
+
+        try{
+            File file = new File("diccionario.txt");
+            Scanner input = new Scanner (file);
+            while(input.hasNextLine()){
+                String dato = input.nextLine();
+                String[] partes = dato.split(",");
+                String ingles = partes[0];
+                String español = partes[1];
+                String frances = partes[2];
+
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
